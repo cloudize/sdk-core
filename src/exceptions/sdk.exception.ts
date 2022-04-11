60c087ae950e6b2fb55b9d7d6654bc2320def6fe
Erich Kuba <erich.kuba@api.games>
2022-04-11 14:51:06 +1200

    Cosmetic cleanup

--- src/exceptions/sdk.exception.ts
+++ src/exceptions/sdk.exception.ts
@@ -1,15 +1,15 @@
 import { ISDKException } from '..';
 
 export default class SDKException extends Error implements ISDKException {
-    private _code: string = '';
+  private _code: string = '';
 
-    constructor(code: string, message: string) {
-      super(message);
-      this.name = 'SDKException';
-      this._code = code;
-    }
+  constructor(code: string, message: string) {
+    super(message);
+    this.name = 'SDKException';
+    this._code = code;
+  }
 
-    get code(): string {
-      return this._code;
-    }
+  get code(): string {
+    return this._code;
+  }
 }
