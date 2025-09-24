3e558d4f781168a15f6c8239096dd190ed78992d
Erich Kuba <erich.kuba@cloudize.net>
2025-09-25 09:42:02 +1200

    This is the first version of v25.1, which utilises NodeJS v24 and updates all dependencies to latest. Initialised the version and published.

--- tests/dev/customer.order.resource.test.ts
+++ tests/dev/customer.order.resource.test.ts
@@ -747,7 +747,7 @@ describe('The customer orders resource ', () => {
             'Microsoft Windows operating systems. It is the successor to Windows 95, and was released to ',
             'manufacturing on May 15, 1998, and generally to retail on June 25, 1998.',
           ],
-          releaseDate: dateUtils.parse('1995-04-28T00:19:39.182', 'YYYY-MM-DDTHH:mm:ss.SSS', true),
+          releaseDate: new Date('1995-04-28T00:19:39.182Z'),
           releaseLocation: {
             longitude: -124.283728,
             latitude: 25.298189,
